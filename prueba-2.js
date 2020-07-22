0051d27144b3d3eec23b1df446a4c20dced2deaf
DANTE <drante12@hotmail.com>
2020-07-21 22:32:07 -0300

    agregamos el segundo comentario

--- prueba-2.js
+++ prueba-2.js
@@ -2,4 +2,5 @@ console.log("buenas noches,Dante")
 console.log ("agregamos otro comentario")
 console.log("agregamos ooootro comentario")
 console.log("entonces, funcionó") // este es un comentario
+console.log("entonces, funcionó") // este es el segundo comentario
  
